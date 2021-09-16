df4eafe3b3e6f4954359987dd682c2dfb83b11a2
TevDickerson <tevbotbhs@gmail.com>
2021-09-16 21:52:55 +0000

    Tennisbot workingspace first files

---
+++ src/video_testing/scripts/OpenVideoRos.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rospy
+import cv2
+
+
+def startvideo():
+    rospy.init_node('videoer', anonymous=True)
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+    
+    cap.set(3, 640)
+    cap.set(4, 480)
+    cap.set(10, 50)
+    
+    #cv2.namedWindow("Video",cv2.WINDOW_NORMAL)
+
+    while not rospy.is_shutdown():
+        success, img = cap.read()
+        cv2.imshow("Video", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+        
+
+if __name__ == '__main__':
+    try:
+        startvideo()
+    except rospy.ROSInterruptException:
+        pass
