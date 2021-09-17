192e793f7cd57471a1c475b3c5c442b7026f96b6
TevDickerson <tevbotbhs@gmail.com>
2021-09-16 20:24:34 -0600

    Add Servo movement testing

--- src/video_testing/scripts/ServoTesting.py
+++ src/video_testing/scripts/ServoTesting.py
@@ -1,6 +1,17 @@
-import board
-import busio
-import adafruit_pca9685
 
-i2c = busio.I2C(board.SCL, board.SDA)
-hat = adafruit_pca9685.PCA9685(i2c)
+import time
+
+from adafruit_servokit import ServoKit
+
+# i2c = busio.I2C(board.SCL, board.SDA)
+# hat = adafruit_pca9685.PCA9685(i2c)
+
+kit = ServoKit(channels=16)
+
+kit.servo[3].angle = 180
+kit.continuous_servo[4].throttle = 1
+time.sleep(1)
+kit.continuous_servo[4].throttle = -1
+time.sleep(1)
+kit.servo[3].angle = 0
+kit.continuous_servo[4].throttle = 0
