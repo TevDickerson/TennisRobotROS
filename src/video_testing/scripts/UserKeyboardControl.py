3b1f51ce6aa40246fc7925f2a540e88661b99917
TevDickerson <tevbotbhs@gmail.com>
2021-09-21 15:28:11 -0600

    Add userKeyboard input new script to subscribe to test node

--- src/video_testing/scripts/UserKeyboardControl.py
+++ src/video_testing/scripts/UserKeyboardControl.py
@@ -1,19 +1,20 @@
-from curses import wrapper
+#!/usr/bin/env python
+
+import rospy
 
 
-def main(stdscr):
-    # Clear screen
-    stdscr.clear()
-
-    # This raises ZeroDivisionError when i == 10.
-    for i in range(0, 11):
-        v = i - 10
-        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
-
-    stdscr.refresh()
-    stdscr.getkey()
+from geometry_msgs.msg import Twist
 
 
-wrapper(main)
+def listener():
+    rospy.init_node('cmd_vel_listener', anonymous=True)
+    rospy.Subscriber("/cmd_vel", Twist)
+    rospy.spin()
 
 
+if __name__ == '__main__':
+
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        pass
