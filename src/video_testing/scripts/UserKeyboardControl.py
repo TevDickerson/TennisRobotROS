50add1774269535c0cef36915bb24d6f69f92284
TevDickerson <tevbotbhs@gmail.com>
2021-09-25 11:12:36 -0600

    Add userKeyboard input throttle

--- src/video_testing/scripts/UserKeyboardControl.py
+++ src/video_testing/scripts/UserKeyboardControl.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 import rospy
+from adafruit_servokit import ServoKit
 
 from geometry_msgs.msg import Twist
+
+kit = ServoKit(channels=16)
 
 
 def callback(msg):
     print(msg.linear.x)
+    kit.continuous_servo[0].throttle = msg.linear.x
 
 
 def listener():
