5abfb6243e4076ac0682687bcb746fbe3a8f56e3
TevDickerson <tevbotbhs@gmail.com>
2021-09-28 13:32:30 -0600

    Add userKeyboard duty cycle control

--- src/video_testing/scripts/UserKeyboardControl.py
+++ src/video_testing/scripts/UserKeyboardControl.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
-
+import board
+import busio
+import adafruit_pca9685
 import rospy
 from adafruit_servokit import ServoKit
 import RPi.GPIO as GPIO
 from geometry_msgs.msg import Twist
 
-kit = ServoKit(channels=16)
+
+i2c = busio.I2C(board.SCL, board.SDA)
+hat = adafruit_pca9685.PCA9685(i2c)
+# kit = ServoKit(channels=16)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 GPIO.output(18, GPIO.HIGH)
 GPIO.output(22, GPIO.HIGH)
+hat.frequency = 60
+RIGHT_WHEEL = hat.channels[0]
+LEFT_WHEEL = hat.channels[1]
 
 
 def callback(msg):
     print(msg.linear.x)
-    kit.continuous_servo[0].throttle = msg.linear.x * 2
-    kit.continuous_servo[1].throttle = msg.linear.x * 2
+    RIGHT_WHEEL.duty_cycle = msg.linear.x * 2 * 1000
+    LEFT_WHEEL.duty_cycle = msg.linear.x * 2 * 1000
 
 
 def listener():
@@ -28,6 +36,9 @@ def listener():
 def myhook():
     GPIO.output(18, GPIO.LOW)
     GPIO.output(22, GPIO.LOW)
+    RIGHT_WHEEL.duty_cycle = 0
+    LEFT_WHEEL.duty_cycle = 0
+
 
 rospy.on_shutdown(myhook)
 
