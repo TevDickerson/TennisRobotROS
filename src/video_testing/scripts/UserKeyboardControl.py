2935a12d66250516f14c9b4b5b73304689854abb
TevDickerson <tevbotbhs@gmail.com>
2021-09-28 16:32:31 -0600

    forward and reverse

--- src/video_testing/scripts/UserKeyboardControl.py
+++ src/video_testing/scripts/UserKeyboardControl.py
@@ -30,12 +30,12 @@ def callback(msg):
     if msg.linear.x >= 0:
         GPIO.output(18, GPIO.HIGH)
         GPIO.output(22, GPIO.HIGH)
-        GPIO.setup(4, GPIO.LOW)
+        GPIO.setup(17, GPIO.LOW)
         GPIO.setup(27, GPIO.LOW)
     else:
         GPIO.output(18, GPIO.LOW)
         GPIO.output(22, GPIO.LOW)
-        GPIO.setup(4, GPIO.HIGH)
+        GPIO.setup(17, GPIO.HIGH)
         GPIO.setup(27, GPIO.HIGH)
 
     throttle_val = abs(msg.linear.x)
@@ -53,7 +53,7 @@ def listener():
 def myhook():
     GPIO.output(18, GPIO.LOW)
     GPIO.output(22, GPIO.LOW)
-    GPIO.setup(4, GPIO.LOW)
+    GPIO.setup(17, GPIO.LOW)
     GPIO.setup(27, GPIO.LOW)
     RIGHT_WHEEL.duty_cycle = 0
     LEFT_WHEEL.duty_cycle = 0
@@ -67,5 +67,5 @@ if __name__ == '__main__':
     try:
         listener()
     except rospy.ROSInterruptException:
-        GPIO.output(4, GPIO.LOW)
+
         pass
